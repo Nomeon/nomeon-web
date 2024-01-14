c1bb39bd07fcc7ace9e610e176808171db17eada
Stijn Nijhuis <spg.nijhuis@hotmail.com>
2024-01-14 20:49:29 +0100

    Sending emails works, display success message on client side

--- src/routes/contact/+page.server.ts
+++ src/routes/contact/+page.server.ts
@@ -2,6 +2,9 @@ import type { PageServerLoad, Actions } from './$types';
 import { fail } from '@sveltejs/kit';
 import { superValidate } from 'sveltekit-superforms/server';
 import { formSchema } from './schema';
+import nodemailer from 'nodemailer';
+import { MAIL_HOST, MAIL_USER, MAIL_PASS, MAIL_SENDER } from '$env/static/private';
+
 export const load: PageServerLoad = async () => {
 	return {
 		form: await superValidate(formSchema)
@@ -15,6 +18,26 @@ export const actions: Actions = {
 				form
 			});
 		}
+
+        const transporter = nodemailer.createTransport({
+            host: MAIL_HOST,
+            port: 465,
+            secure: true,
+            auth: {
+                type: "LOGIN",
+                user: MAIL_SENDER,
+                pass: MAIL_PASS
+            }
+        })
+        const sendMail = await transporter.sendMail({
+            from: `Contactform Nomeon <${MAIL_SENDER}>`,
+            to: MAIL_USER,
+            subject: `${form.data.name} via contactform`,
+            text: `Naam: ${form.data.name}\nEmail: ${form.data.email}\nBericht: ${form.data.message}`
+        })
+        console.log(sendMail.messageId)
+        transporter.close()
+
 		return {
 			form
 		};
